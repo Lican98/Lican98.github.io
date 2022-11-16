39d9c2391a0fefee6b49f6681d1858e0d52e9e24
lican <lican.m.olea@gmail.com>
2022-11-16 09:29:54 -0300

    redes sociales

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ function App() {
           <img src="/logolican.png" width="80" height="80" alt="" />
           <form class="d-flex">
             <a href='#lenguaje' class="btn  rounded-pill px-3 mb-2 mb-lg-0">Lenguajes Aprendidos</a>
-            <a href='#informacion' class="btn  rounded-pill px-3 mb-2 mb-lg-0" >Información Personal</a>
             <a href='#redes' class="btn  rounded-pill px-3 mb-2 mb-lg-0" >Redes Sociales</a>
           </form>
         </div>
@@ -24,17 +23,20 @@ function App() {
             <div class="col-5">
               <div class="card">
                 <center>
-                  <img class="logo" src="/logolican.png" width="150" height="200" alt="" />
+                  <img class="logo" src="/logolican.png" width="150" height="150" alt="" />
                 </center>
                 <br></br>
                 <p class="card-title">Lican Santander</p>
               </div>
             </div>
             <div class="col-6">
-              <div class="row">
-                <p class="center h5">
-                  Desarrolladora de citio web. En buscqueda de un buen ambiente laboral con grandes desafio y mas que todo con muchas ganas de aprender cosas nuevas , donde la cratividad sea un elemento fundamental para el desarrollo del trabajo tanto individual como en el equipo.
-                </p>
+              <div class="margin">
+                <div class="row">
+                  <p class="center h5">
+                    Desarrolladora de citio web. En buscqueda de un buen ambiente laboral con grandes desafio y mas que todo con muchas ganas de aprender cosas nuevas , donde la cratividad sea un elemento fundamental para el desarrollo del trabajo tanto individual como en el equipo.
+                  </p>
+                  <a href="Curriculum.pdf" download="Acme Documentation (ver. 2.0.1).txt">Descarga de Curriculum</a>
+                </div>
               </div>
             </div>
           </div>
@@ -47,21 +49,21 @@ function App() {
       <div class="container">
         <div class="card-group">
           <div class="card" >
-            <img src="/HTML.png" alt="..." />
+            <img class="imagem" src="/HTML.png" alt="..." />
             <div class="card-b">
               <h5 class="card-title">HTML</h5>
               <p class="card-text">El Lenguaje de Marcado de Hipertexto (HTML) es el código que se utiliza para estructurar y desplegar una página web y sus contenidos.</p>
             </div>
           </div>
           <div class="card">
-            <img src="/CSS.png" alt="..." />
+            <img class="imagem" src="/CSS.png" alt="..." />
             <div class="card-b">
               <h5 class="card-title">CSS</h5>
               <p class="card-text"> CSS es un lenguaje que maneja el diseño y presentación de las páginas web, es decir, cómo lucen cuando un usuario las visita. </p>
             </div>
           </div>
           <div class="card">
-            <img src="/JS.png" alt="..." />
+            <img class="imagem" src="/JS.png" alt="..." />
             <div class="card-b">
               <h5 class="card-title">JS</h5>
               <p class="card-text">JavaScript es un lenguaje de programación que los desarrolladores utilizan para hacer páginas web interactivas.</p>
@@ -73,21 +75,21 @@ function App() {
       <div class="container">
         <div class="card-group">
           <div class="card">
-            <img src="/git.png" alt="..." />
+            <img class="imagem" src="/git.png" alt="..." />
             <div class="card-b">
               <h5 class="card-title">GIT</h5>
               <p class="card-text"> Git es, con diferencia, el sistema de control de versiones moderno más utilizado del mundo.</p>
             </div>
           </div>
           <div class="card">
-            <img src="/boostrap.png" alt="..." />
+            <img class="imagem" src="/boostrap.png" alt="..." />
             <div class="card-b">
               <h5 class="card-title">BOOSTRAP</h5>
               <p class="card-text">Bootstrap es una biblioteca multiplataforma o conjunto de herramientas de código abierto para diseño de sitios y aplicaciones web.</p>
             </div>
           </div>
           <div class="card">
-            <img src="/github.png" alt="..." />
+            <img class="imagem" src="/github.png" alt="..." />
             <div class="card-b">
               <h5 class="card-title">GITHUB</h5>
               <p class="card-text">GitHub es una forja para alojar proyectos utilizando el sistema de control de versiones Git. Se utiliza principalmente para la creación de código fuente de programas de ordenador.</p>
@@ -97,9 +99,23 @@ function App() {
       </div>
       <br></br>
       <h2 id='redes'> Redes Sociales:</h2>
-      <embed src="./Curriculum.pdf" type="application/pdf" width="100%" height="600px" />
+      <div class="container">
+        <center>
+          <div class="col-md-4">
+            <a href="https://github.com/Lican98" class="btn  rounded-pill px-3 mb-2 mb-lg-0">
+              <img src="/github-logo.png" width="80" height="80" alt="" />
+            </a>
+            <br></br>
+            <a href="mailto:lican.m.olea@gmail.com" class="btn  rounded-pill px-3 mb-2 mb-lg-0">
+              <img src="/Gmail.png" width="80" height="80" alt="" />
+            </a>
+            <a href="https://www.linkedin.com/in/lican-santander-olea-9736a711a/" class="btn  rounded-pill px-3 mb-2 mb-lg-0">
+              <img src="/linkedin.png" width="80" height="80" alt="" />
+            </a>
+          </div>
+        </center>
+      </div>
       <br></br>
-
       <footer class="bg-black text-center py-5">
         <div class="container px-5">
           <div class="text-white-50 small">
